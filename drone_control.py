8e0e76613d10caed8dd18bcafbb1f219b97a435f
Drakula44 <ninadrakulic04@gmail.com>
2024-09-26 23:58:35 +0200

    updated drone control

--- drone_control.py
+++ drone_control.py
@@ -1,9 +1,23 @@
 from pymavlink import mavutil
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
+import numpy as np
+from ignition.msgs.image_pb2 import Image
+from gz.transport13 import Node
+import cv2
+import time
+import numpy as np
 
 
-def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration=0):
+def stringmsg_cb(msg):
+    print("Received message:")
+    print(msg.height, msg.width)
+    im = np.frombuffer(msg.data, dtype=np.uint8).reshape((msg.height, msg.width, 3))
+    cv2.imshow("Image", im)
+    cv2.waitKey(25)
+
+
+def send_body_ned_velocity(vehicle, velocity_x, velocity_y, velocity_z, duration=1):
     msg = vehicle.message_factory.set_position_target_local_ned_encode(
         0,  # time_boot_ms (not used)
         0,
@@ -24,39 +38,66 @@ def send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration=0):
     )
     for x in range(0, duration):
         vehicle.send_mavlink(msg)
+        # time.sleep(1)
+
+
+def move(vehicle):
+    start_time = time.time()
+    while True:
+        r = 5
+        x_speed = -np.sin((time.time() - start_time) / r) * 5
+        y_speed = np.cos((time.time() - start_time) / r) * 5
+        print("x_speed: ", x_speed)
+        print("y_speed: ", y_speed)
+
+        send_body_ned_velocity(vehicle, x_speed, y_speed, 0)
+        if time.time() - start_time > 20:
+            send_body_ned_velocity(vehicle, 0, 0, 0)
+            print("STOPPED")
+            break
+
+
+def land(vehicle):
+    vehicle.mode = VehicleMode("LAND")
+
+
+def takeoff(vehicle, takeoff_alt):
+    vehicle.mode = VehicleMode("GUIDED")
+    vehicle.armed = True
+    while not vehicle.armed:
+        print("Waiting for arming...")
+        time.sleep(1)
+    vehicle.simple_takeoff(takeoff_alt)  # Take off to target altitude
+    while True:
+        print("Altitude: %d" % vehicle.location.global_relative_frame.alt)
+        if vehicle.location.global_relative_frame.alt >= takeoff_alt * 0.95:
+            print("REACHED TARGET ALTITUDE")
+            break
+        time.sleep(0.5)
+
+
+def main():
+    connection_string = "127.0.0.1:14550"
+    takeoff_alt = 10
+    vehicle = connect(connection_string, wait_ready=True)
+
+    node = Node()
+    topic_img = "/default/iris/iris_demo/gimbal_small_2d/tilt_link/camera/image"
+
+    # subscribe to a topic by registering a callback
+    if node.subscribe(Image, topic_img, stringmsg_cb):
+        print("Subscribing to type {} on topic [{}]".format(Image, topic_img))
+    else:
+        print("Error subscribing to topic [{}]".format(topic_img))
+        return
+
+    while not vehicle.is_armable:
         time.sleep(1)
 
+    takeoff(vehicle, takeoff_alt)
+    move(vehicle)
+    land(vehicle)
 
-connection_string = "127.0.0.1:14550"
-takeoff_alt = 10
-vehicle = connect(connection_string, wait_ready=True)
-while not vehicle.is_armable:
-    time.sleep(1)
-vehicle.mode = VehicleMode("GUIDED")
-vehicle.armed = True
-while not vehicle.armed:
-    print("Waiting for arming...")
-    time.sleep(1)
-vehicle.simple_takeoff(takeoff_alt)  # Take off to target altitude
-while True:
-    print("Altitude: %d" % vehicle.location.global_relative_frame.alt)
-    if vehicle.location.global_relative_frame.alt >= takeoff_alt * 0.95:
-        print("REACHED TARGET ALTITUDE")
-        break
-    time.sleep(1)
 
-# This is the command to move the copter 5 m/s forward for 10 sec.
-velocity_x = 0
-velocity_y = 5
-velocity_z = 0
-duration = 10
-send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration)
-
-# backwards at 5 m/s for 10 sec.
-velocity_x = 0
-velocity_y = -5
-velocity_z = 0
-duration = 10
-send_body_ned_velocity(velocity_x, velocity_y, velocity_z, duration)
-
-vehicle.mode = VehicleMode("LAND")
+if __name__ == "__main__":
+    main()
