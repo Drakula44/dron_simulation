c88658db885ccec43a54a3eb2c5ffb836dcf1e0a
Drakula44 <ninadrakulic04@gmail.com>
2024-09-26 18:58:23 +0200

    modified activate_env.sh for the ubuntu machines

--- activate_env.sh
+++ activate_env.sh
@@ -1,6 +1,7 @@
 conda activate pfe-dron
 chmod +x ~/.conda/envs/pfe-dron/share/gazebo/setup.sh
-source ~/.conda/envs/pfe-dron/share/gazebo/setup.sh
+export CONDA_ENV_PATH=~/miniconda/envs/pfe-dron
+source $CONDA_ENV_PATH/share/gazebo/setup.sh
 export PYTHONPATH="${PYTHONPATH}:$HOME/pfe-dron/gz-msgs/python-proto"
 export GAZEBO_MODEL_PATH=~/pfe-dron/ardupilot_gazebo/models:${GAZEBO_MODEL_PATH}
 export GAZEBO_MODEL_PATH=~/pfe-dron/ardupilot_gazebo/models_gazebo:${GAZEBO_MODEL_PATH}
